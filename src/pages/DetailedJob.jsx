02ad5720bbb638adef566a89e39ef74cdd42a496
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-11-22 15:22:43 +0200

    edits

--- src/pages/DetailedJob.jsx
+++ src/pages/DetailedJob.jsx
@@ -5,6 +5,7 @@ import { useLocation, useParams } from 'react-router-dom';
 import GoogleMap from 'components/GoogleMap';
 import { BsBookmark } from 'react-icons/bs';
 import { BsFillShareFill } from 'react-icons/bs';
+import timeSince from 'utils';
 
 const DetailedJob = () => {
   const { jobId } = useParams();
@@ -18,7 +19,7 @@ const DetailedJob = () => {
       {job && (
         <Section className="max-w-7xl m-auto">
           <div className="flex">
-            <div className="w-3/5">
+            <div className="w-3/5 mr-24">
               <header className="border-b">
                 <div className="flex justify-between ">
                   <Link
@@ -47,26 +48,48 @@ const DetailedJob = () => {
                 </div>
               </header>
               <body>
-                <div>
+                <div className="text-[#3A4562]">
                   <button
                     type="button"
                     className="w-[127px] h-[58px] bg-[#2A3047] rounded-lg text-white font-semibold text-xs uppercase mt-10 mb-9  hover:text-violet-600"
                   >
                     Apply now
                   </button>
-                  <h1 className="w-[501] h-auto">{job.title}</h1>
-                  <h2>{job.salary} Brutto, per year</h2>
-                  <p>{job.createdAt}</p>
+                  <div className="flex justify-between">
+                    <h1 className="w-[501] h-auto font-bold text-2xl mb-2 mr-14 ">
+                      {job.title}
+                    </h1>
+                    <p className="font-bold text-2xl min-w-[161px]">
+                      {job.salary}
+                      <br />
+                      <span className="font-normal text-lg">
+                        Brutto, per year
+                      </span>
+                    </p>
+                  </div>
+                  <p className="text-slate-400">
+                    Posted &nbsp;
+                    {timeSince(Date.parse(job.createdAt))} ago
+                  </p>
                   <p>{job.description}</p>
-                  <button type="button">Apply now</button>
+                  <button
+                    type="button"
+                    className="w-[127px] h-[58px] bg-[#2A3047] rounded-lg text-white font-semibold text-xs uppercase mt-10 mb-9  hover:text-violet-600"
+                  >
+                    Apply now
+                  </button>
                 </div>
-                <div>
-                  <h2>Additional info</h2>
-                  <p>Employment type</p>
+                <div className="text-[#3A4562]">
+                  <h2 className="font-bold text-3xl">Additional info</h2>
+                  <p className="text-lg font-normal">Employment type</p>
                   {job.employment_type.map(e => (
-                    <div>{e}</div>
+                    <ul>
+                      <li className="w-[222px] h-[49px] border-2 border-[#55699e/30] rounded-lg">
+                        {e}
+                      </li>
+                    </ul>
                   ))}
-                  <p>Benefits</p>
+                  <p className="text-lg font-normal">Benefits</p>
                   {job.benefits.map(e => (
                     <div>{e}</div>
                   ))}
@@ -87,17 +110,19 @@ const DetailedJob = () => {
               </body>
               <button type="button">RETURN TO JOB BOARD</button>
             </div>
-            <div className="relative z-30 min-w-[402px] w={584} h={728} bg-[#2A3047] overflow-hidden px-16 pt-8 pb-4  text-[#E8EBF3]">
+            <div className="flex flex-col justify-between relative z-30 w-[402px] max-h-[436px] bg-[#2A3047] overflow-hidden  text-[#E8EBF3]">
               <div
                 objectFit="cover"
                 layout="fill"
-                className="absolute -translate-x-3/4  bg-[#202336] rounded-full w-80 h-80  -z-10"
+                className="absolute -translate-x-1/4  bg-[#202336] rounded-full w-80 h-80  -z-10"
               ></div>
-              <h2 className="z-10 font-bold text-xl">{job.name}</h2>
-              <p className="z-10 font-normal text-lg">{job.address}</p>
-              <p className="z-10 font-normal text-lg">{job.phone}</p>
-              <p className="z-10 font-normal text-lg">{job.email}</p>
-              <div>
+              <div className="px-16 pt-8 pb-4">
+                <h2 className="z-10 font-bold text-xl">{job.name}</h2>
+                <p className="z-10 font-normal text-lg">{job.address}</p>
+                <p className="z-10 font-normal text-lg">{job.phone}</p>
+                <p className="z-10 font-normal text-lg">{job.email}</p>
+              </div>
+              <div className="flex-none">
                 <GoogleMap location={job.location} />
               </div>
             </div>
